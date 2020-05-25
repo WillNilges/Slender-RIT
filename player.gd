8bf5b2f5291f4da41a71c445bf10dd4a0f50dbc3
Bootskiing <connortheguy12@gmail.com>
2020-05-25 03:18:33 -0400

    Base map; big square

--- player.gd
+++ player.gd
@@ -30,24 +30,24 @@ func _ready():
 # called when an input is detected
 func _input (event):
 
-    # did the mouse move?
-    if event is InputEventMouseMotion:
-        mouseDelta = event.relative
+	# did the mouse move?
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
 		
 # called every frame
 func _process (delta):
 
-    # rotate camera along X axis
-    camera.rotation_degrees -= Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
+	# rotate camera along X axis
+	camera.rotation_degrees -= Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
 
-    # clamp the vertical camera rotation
-    camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
+	# clamp the vertical camera rotation
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
 	
-    # rotate player along Y axis
-    rotation_degrees -= Vector3(0, rad2deg(mouseDelta.x), 0) * lookSensitivity * delta
+	# rotate player along Y axis
+	rotation_degrees -= Vector3(0, rad2deg(mouseDelta.x), 0) * lookSensitivity * delta
 	
-    # reset the mouse delta vector
-    mouseDelta = Vector2()
+	# reset the mouse delta vector
+	mouseDelta = Vector2()
 
 # called every physics step
 func _physics_process (Delta):
@@ -59,13 +59,13 @@ func _physics_process (Delta):
 	
 	# movement inputs
 	if Input.is_action_pressed("move_forward"):
-	    input.y -= 1
+		input.y -= 1
 	if Input.is_action_pressed("move_backward"):
-	    input.y += 1
+		input.y += 1
 	if Input.is_action_pressed("move_left"):
-	    input.x -= 1
+		input.x -= 1
 	if Input.is_action_pressed("move_right"):
-	    input.x += 1
+		input.x += 1
 	
 	# normalize the input so we can't move faster diagonally
 	input = input.normalized()
@@ -86,33 +86,33 @@ func _physics_process (Delta):
 	
 	# jump if we press the jump button and are standing on the floor
 	if Input.is_action_pressed("jump") and is_on_floor():
-	    vel.y = jumpForce
+		vel.y = jumpForce
 		
 #=====
 # called when an enemy damages us
 func take_damage (damage):
 
-    curHp -= damage
+	curHp -= damage
 
-    if curHp <= 0:
-        die()
+	if curHp <= 0:
+		die()
 
 # called when our health reaches 0
 func die ():
 
-    pass
+	pass
 
 # called when we kill an enemy
 func add_score (amount):
 
-    score += amount
+	score += amount
 
 # adds an amount of health to the player
 func add_health (amount):
 
-    curHp = clamp(curHp + amount, 0, maxHp)
+	curHp = clamp(curHp + amount, 0, maxHp)
 
 # adds an amount of ammo to the player
 func add_ammo (amount):
 
-    ammo += amount
+	ammo += amount
